35037e0c4d8ccca58feb3ade10b0211a9c9c8b9a
Ian Baker <raindrift@dropbox.com>
2020-08-01 19:36:40 -0700

    little monitoring fixes

--- app/controllers/call_status_controller.rb
+++ app/controllers/call_status_controller.rb
@@ -5,11 +5,12 @@ class CallStatusController < ApplicationController
     client_number = redis.get("caller-#{sid}")
     number_stripped = strip_number(client_number)
     label = params['label'].to_sym
+    call_count = redis.get("call_count-#{strip_number(client_number)}")
 
     if after_hours? label
       sms client_number, "The number we were calling is now closed. Try again soon? #{call_count_report(client_number)}"
-      call_count = redis.get("call_count-#{strip_number(client_number)}")
       redis.lpush("failures-#{label}", "#{Time.now.strftime('%Y%m%d%H%M%S')}:#{call_count}")
+      sms "+14152408408", "Failed after hours for #{number_stripped} / #{label} / #{call_count} calls" # janky monitoring
       return
     end
 
@@ -30,7 +31,6 @@ class CallStatusController < ApplicationController
       call_edd label, client_number, false
     else
       sms client_number, "Looks like maybe you got through? We'll stop trying, but you can restart any time (reply with MAIN or ONLINE). #{call_count_report(client_number)}"
-      call_count = redis.get("call_count-#{strip_number(client_number)}")
       redis.lpush("successes-#{label}", "#{Time.now.strftime('%Y%m%d%H%M%S')}:#{call_count}")
       redis.del("active-#{number_stripped}")
       sms "+14152408408", "Success for #{number_stripped} / #{label} / #{call_count} calls / #{params['Duration']} min" # janky monitoring
